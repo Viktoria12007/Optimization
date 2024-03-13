73b9ed2441b1c1cebdd7c2a31faf0dc6d510f670
Виктория Голубова <v.golubova@denexy.com>
2024-03-13 17:58:44 +0400

    Start project.

---
+++ src/components/Team/Team.tsx
@@ -0,0 +1,23 @@
+import { ITeam } from "../../api.ts";
+import "./styles.css";
+
+interface ITeamData {
+  team: ITeam[];
+}
+export const Team = ({ team }: ITeamData) => {
+  return (
+    <>
+      <h2>Our Team</h2>
+      <div className="component">
+        {team.map(({ name, position, image }) => (
+          <div key={name}>
+            <img src={image} width={300} height={300} alt="" />
+            <p>
+              {name}: {position}
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
